5fc7862aa59214c9ef16a24ad65bf915d44f0a47
Dheeraj S <dheerajstvpm@gmail.com>
2023-11-18 21:37:31 +0530

    basic chat app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Peer } from "peerjs";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor() { }
+
   title = 'demo-chat';
+
+  peer: Peer | undefined;
+  message = ''
+  messages: string[] = [];
+
+  createPeer() {
+    if (!this.peer) {
+      this.peer = new Peer("maanyanaaya-dheeraj");
+      this.peer;
+    }
+  }
+
+  receive() {
+    if (this.peer) {
+      this.peer.on("connection", (conn) => {
+        conn.on("data", (data) => {
+          this.messages = [...this.messages, data as string]
+          console.log(data);
+        });
+      });
+    }
+  }
+
+  send() {
+    if (this.peer) {
+      const conn = this.peer.connect("alavalaathi-rubeena");
+      conn.on("open", () => {
+        conn.send(this.message);
+      });
+    }
+  }
 }
