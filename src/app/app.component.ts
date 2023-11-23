c9654f2559d146c00946a7dc00bba04d844fe8fd
Dheeraj S <dheerajstvpm@gmail.com>
2023-11-23 09:17:07 +0530

    include all messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { Peer } from "peerjs";
 
+export interface IMessage {
+  peer: string,
+  time: Date,
+  message: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
 
   constructor() { }
@@ -14,12 +21,13 @@ export class AppComponent {
 
   peer: Peer | undefined;
   message = ''
-  messages: string[] = [];
-  otherPeerId = '';
+  messages: IMessage[] = [];
+  myPeerId = "maanyanaaya-dheeraj";
+  otherPeerId = "alavalaathi-rubeena";
 
   createPeer() {
     if (!this.peer) {
-      this.peer = new Peer("maanyanaaya-dheeraj");
+      this.peer = new Peer(this.myPeerId);
       this.receive();
     }
   }
@@ -28,7 +36,13 @@ export class AppComponent {
     if (this.peer) {
       this.peer.on("connection", (conn) => {
         conn.on("data", (data) => {
-          this.messages = [...this.messages, data as string]
+          const time = new Date();
+          const peerMessage: IMessage = {
+            peer: 'sender',
+            time: time,
+            message: data as string
+          };
+          this.messages = [...this.messages, peerMessage]
           console.log(data);
         });
       });
@@ -36,10 +50,21 @@ export class AppComponent {
   }
 
   send() {
+    if (this.message === '') {
+      return;
+    }
     if (this.peer) {
+      const time = new Date();
+      const myMessage: IMessage = {
+        peer: 'me',
+        time: time,
+        message: this.message
+      };
+      this.messages = [...this.messages, myMessage]
       const conn = this.peer.connect(this.otherPeerId);
       conn.on("open", () => {
         conn.send(this.message);
+        this.message = '';
       });
     }
   }
