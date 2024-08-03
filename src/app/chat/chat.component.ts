f0466d0520701571b52edfe6752ce02f956f6511
ds097104 <dsanthosh@ep.com>
2024-08-04 03:31:28 +0530

    update calling

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,6 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import Peer from 'peerjs';
+import Peer, { MediaConnection } from 'peerjs';
 import { FormsModule } from '@angular/forms';
 import { CdkDrag } from '@angular/cdk/drag-drop';
 
@@ -17,7 +17,7 @@ export interface IMessage {
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss',
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit {
 
   @ViewChild('localVideo') localVideo!: ElementRef<HTMLVideoElement>;
   @ViewChild('remoteVideo') remoteVideo!: ElementRef<HTMLVideoElement>;
@@ -26,54 +26,104 @@ export class ChatComponent {
 
   peer: Peer | undefined;
   message = '';
-  messages: IMessage[] = [];
-  myPeerId = crypto.randomUUID();
-  otherPeerId!: string;
+  set messages(value: IMessage[]) {
+    const messages = JSON.stringify(value);
+    localStorage.setItem(`messages`, messages);
+  }
+  get messages() {
+    let messages;
+    try {
+      messages = JSON.parse(localStorage.getItem(`messages`) ?? 'null');
+    } catch (error) {
+      console.log(error);
+      messages = [];
+    }
+    return messages ?? [];
+  }
+  myPeerId = localStorage.getItem(`peerId`) ?? crypto.randomUUID();
+  set otherPeerId(id: string) {
+    localStorage.setItem(`otherPeerId`, id);
+  }
+  get otherPeerId() { return localStorage.getItem(`otherPeerId`) ?? `` };
   callConnected = false;
-  muted = false;
+  audioEnabled = true;
+  videoEnabled = false;
+
+  ngOnInit(): void {
+    this.createPeer();
+  }
 
   createPeer() {
     if (!this.peer) {
+      localStorage.setItem(`peerId`, this.myPeerId);
       this.peer = new Peer(this.myPeerId);
       this.onReceive();
       this.onCall();
+      this.onPeerDisconnect();
     }
+  }
+
+  async setLocalStream() {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: this.videoEnabled,
+      audio: this.audioEnabled,
+    });
+    this.localVideo.nativeElement.srcObject = stream;
+    return stream;
+  }
+
+  setRemoteStream(mediaConnection: MediaConnection) {
+    mediaConnection.on('stream', (remoteStream) => {
+      this.remoteVideo.nativeElement.srcObject = remoteStream;
+      this.callConnected = true;
+    });
   }
 
   onReceive() {
-    if (this.peer) {
-      this.peer.on('connection', (conn) => {
-        conn.on('data', (data) => {
-          const time = new Date();
-          const peerMessage: IMessage = {
-            peer: 'sender',
-            time: time,
-            message: data as string,
-          };
-          this.messages = [...this.messages, peerMessage];
-          console.log(data);
-        });
+    this.peer?.on('connection', (conn) => {
+      conn.on('data', (data) => {
+        const time = new Date();
+        const peerMessage: IMessage = {
+          peer: 'sender',
+          time: time,
+          message: data as string,
+        };
+        this.messages = [...this.messages, peerMessage];
+        console.log(data);
       });
-    }
+    });
   }
 
   onCall() {
-    if (this.peer) {
-      this.peer.on('call', async (call) => {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          video: true,
-          audio: true,
-        });
-        this.muted = false;
-        this.localVideo.nativeElement.srcObject = stream;
-        call.answer(stream); // Answer the call with an A/V stream.
-        call.on('stream', (remoteStream) => {
-          // Show stream in some <video> element.
-          this.remoteVideo.nativeElement.srcObject = remoteStream;
-          this.callConnected = true;
-        });
-      });
-    }
+    this.peer?.on('call', async (call) => {
+      this.audioEnabled = true;
+      call.answer(await this.setLocalStream());
+      this.setRemoteStream(call);
+      this.onCallDisconnect(call);
+    });
+  }
+
+  onPeerDisconnect() {
+    this.peer?.on('close', () => {
+      this.disconnect();
+    })
+    this.peer?.on('disconnected', () => {
+      this.disconnect();
+    });
+    this.peer?.on('error', () => {
+      this.disconnect();
+    });
+  }
+
+  onCallDisconnect(connection: MediaConnection) {
+    connection.on('close', () => {
+      connection.close();
+      this.disconnect();
+    })
+    connection.on('error', () => {
+      connection.close();
+      this.disconnect();
+    })
   }
 
   send() {
@@ -94,46 +144,46 @@ export class ChatComponent {
       conn.on('open', () => {
         conn.send(message);
       });
+    } else {
+      this.createPeer();
+      this.send();
     }
   }
 
   async call() {
     if (this.peer) {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: true,
-      });
-      this.muted = false;
-      this.localVideo.nativeElement.srcObject = stream;
-      const call = this.peer.call(this.otherPeerId, stream);
-      call.on('stream', (remoteStream) => {
-        // Show stream in some <video> element.
-        this.remoteVideo.nativeElement.srcObject = remoteStream;
-        this.callConnected = true;
-      });
+      this.audioEnabled = true;
+      const call = this.peer.call(this.otherPeerId, await this.setLocalStream());
+      this.setRemoteStream(call);
+      this.onCallDisconnect(call);
+    } else {
+      this.createPeer();
+      this.call();
     }
   }
 
-  async mute() {
-    this.muted = true;
-    const stream = await navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: false,
-    });
-    this.localVideo.nativeElement.srcObject = stream;
+  async toggleMute() {
+    this.audioEnabled = !this.audioEnabled;
+    this.peer?.call(this.otherPeerId, await this.setLocalStream());
   }
 
-  async unmute() {
-    this.muted = false;
-    const stream = await navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: true,
-    });
-    this.localVideo.nativeElement.srcObject = stream;
+  async toggleCamera() {
+    if (this.peer) {
+      this.videoEnabled = !this.videoEnabled;
+      this.peer.call(this.otherPeerId, await this.setLocalStream());
+    }
   }
 
-  disconnect() {
+  async disconnect() {
     this.callConnected = false;
+    this.localVideo.nativeElement.srcObject = null;
+    this.remoteVideo.nativeElement.srcObject = null;
+    const stream = await this.setLocalStream();
+    stream.getTracks().forEach(track => {
+      track.stop();
+      stream.removeTrack(track);
+    })
     this.peer?.destroy();
+    this.peer = undefined;
   }
 }
